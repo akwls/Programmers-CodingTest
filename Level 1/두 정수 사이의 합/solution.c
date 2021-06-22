0b0b699cb9ee1fb7524e0c27565aafc30407e98d
Hajin Kim <rhrnak624@naver.com>
2021-06-22 16:54:32 +0900

    0622

---
+++ Level 1/두 정수 사이의 합/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if(a > b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    for(int i = a; i<=b; i++) {
+        answer += i;
+    }
+    return answer;
+}
