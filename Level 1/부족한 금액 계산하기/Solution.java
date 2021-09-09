3d49ff31f60ce3d74e678954d75724d080cd37f2
Hajin Kim <rhrnak624@naver.com>
2021-09-10 00:44:35 +0900

    0910

---
+++ Level 1/부족한 금액 계산하기/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long sum = 0;
+        for(int i=1; i<= count; i++) {
+            sum += price * i;
+        }
+        answer = sum - money;
+        if(answer <= price) answer = 0;
+        return answer;
+    }
+}
