09a84877c4465cd7830c7e3826f25424ee0567eb
Hajin Kim <rhrnak624@naver.com>
2021-10-01 14:09:48 +0900

    없는 숫자 더하기

---
+++ Level 1/없는 숫자 더하기/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] arr = new int[10];
+        for(int i=0; i<numbers.length; i++) {
+            arr[numbers[i]]++;
+        }
+        for(int i=0; i<10; i++) {
+            if(arr[i] == 0) answer+=i;
+        }
+        return answer;
+    }
+    
+}
